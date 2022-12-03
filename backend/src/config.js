b3ba5a736403232a07fba5318173419d5daaed14
EarthWoodFlow <d_w_sterling@yahoo.com>
2022-12-04 11:40:49 +1300

    update sliders

--- backend/src/config.js
+++ backend/src/config.js
@@ -70,15 +70,15 @@ const CONTRACT_SYMBOL = 'SSGF';
 const METADATA_UPDATABLE = true; // set to false if you don't want to allow metadata updates after minting
 const OWNER_ADDRESS = '0x89c822Ba5aa4222010Bd2881e50fa3Aaf83fa5fC';
 const TREASURY_ADDRESS = '0x89c822Ba5aa4222010Bd2881e50fa3Aaf83fa5fC';
-const MAX_SUPPLY = 5000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
+const MAX_SUPPLY = 10000; // The maximum number of NFTs that can be minted. CANNOT BE UPDATED!
 const MINT_PRICE = 0.001; // Minting price per NFT. Goerli = ETH, Ethereum = ETH, Polygon = MATIC. CANNOT BE UPDATED!
 const TOKENS_PER_MINT = 100; // maximum number of NFTs a user can mint in a single transaction. CANNOT BE UPDATED!
 
 // REQUIRED CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PUBLIC_MINT_START_DATE = "2022-12-01T11:30:48+00:00"; // This is required. Eg: 2022-02-08T11:30:48+00:00
+const PUBLIC_MINT_START_DATE = null; // This is required. Eg: 2022-02-08T11:30:48+00:00
 
 // OPTIONAL CONTRACT DETAILS THAT CAN BE UPDATED LATER.
-const PRESALE_MINT_START_DATE = "2022-12-01T11:30:48+00:00"; // Optional. Eg: 2022-02-08T11:30:48+00:00
+const PRESALE_MINT_START_DATE = null; // Optional. Eg: 2022-02-08T11:30:48+00:00
 const ROYALTY_SHARE = 1000; // Percentage of the token price that goes to the royalty address. 100 bps = 1%
 const ROYALTY_ADDRESS = "0x89c822Ba5aa4222010Bd2881e50fa3Aaf83fa5fC"; // Address that will receive the royalty
 const BASE_URI = null; // only update if you want to manually set the base uri

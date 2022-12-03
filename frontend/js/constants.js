7cd3c48319931e8aa21a1f9218d8765d9bd380b0
EarthWoodFlow <d_w_sterling@yahoo.com>
2022-12-03 20:28:36 +1300

    Update2

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; // Replace with your own contract address
+const contractAddress = "0x31E10690370cc5E54d08798167cE15B318D05007"; // Replace with your own contract address
 const chain = 'goerli'; // goerli, polygon, or ethereum
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
